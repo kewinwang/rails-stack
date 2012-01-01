bce80b5ea0142a65c936800332a7d39be5731f4f
Raecoo Cao <raecoo@gmail.com>
2012-01-01 22:26:05 +0800

    renew the git

--- recipes/init.rb
+++ recipes/init.rb
@@ -1,7 +1,7 @@
 # Build Essential
 package :build_essential, :provides => :init do
   description 'Build tools and libraries, such like build-essential, git and more.'
-  apt 'build-essential zlib1g zlib1g-dev libssl-dev libreadline5-dev libcurl4-openssl-dev' do
+  apt 'build-essential git git-core zlib1g zlib1g-dev libssl-dev libreadline5-dev libcurl4-openssl-dev' do
     pre  :install, "apt-get -y update"
   end
   requires :imagemagick
